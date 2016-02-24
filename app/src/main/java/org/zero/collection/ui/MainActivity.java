54548f6728b3715968f4397db71cff89beb63586
Zero <chfdeng@gmail.com>
2016-02-24 18:13:51 +0800

    update

--- app/src/main/java/org/zero/collection/ui/MainActivity.java
+++ app/src/main/java/org/zero/collection/ui/MainActivity.java
@@ -1,7 +1,9 @@
 package org.zero.collection.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import org.zero.collection.R;
 
@@ -11,5 +13,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(MainActivity.this,CodeScannerActivity.class));
+            }
+        });
     }
 }
